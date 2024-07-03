abb2c9a5b511f9c9bd3f34c0226c47f35cf9fc3f
amantayal2909 <76427320+amantayal2909@users.noreply.github.com>
2024-07-03 09:07:41 +0530

    Time: 64 ms (79.3%), Space: 38.7 MB (80.72%) - LeetHub

--- 1509-minimum-difference-between-largest-and-smallest-value-in-three-moves/1509-minimum-difference-between-largest-and-smallest-value-in-three-moves.cpp
+++ 1509-minimum-difference-between-largest-and-smallest-value-in-three-moves/1509-minimum-difference-between-largest-and-smallest-value-in-three-moves.cpp
@@ -6,18 +6,20 @@ public:
 
         sort(nums.begin(),nums.end());
 
-        int diff=INT_MAX;
-        for(int i=4; i>=1;i--){
-            int j=4-i;
-            while(j>=0){
-                diff=min(diff,nums[n-i]-nums[j]);
-                j--;
-            }
-            
-        }
+        int a=nums[n-4]-nums[0];
+        int b=nums[n-1]-nums[3];
+        int c=nums[n-3]-nums[1];
+        int d=nums[n-2]-nums[2];
 
-    return diff;
+        int e=min(a,b);
+        int f=min(c,d);
+        return min(e,f);  
+        //return min({a,b,c,d});
+        
     }
 };
 
-
+//0 1 5 8 10 14
+//  1 4 3  2  4
+//20 75 81 82 83 95
+//20 75 81 82 95
