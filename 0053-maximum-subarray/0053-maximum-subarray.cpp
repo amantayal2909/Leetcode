64fd783c63b8a30e259cb62251ce27b8737e3e94
amantayal2909 <76427320+amantayal2909@users.noreply.github.com>
2026-03-25 00:11:40 +0530

    Time: 0 ms (100%), Space: 71.7 MB (52.01%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -6,9 +6,9 @@ public:
         int ans = INT_MIN;
         for(int i=0;i<n;i++){
             sum += nums[i];
-            //ans = max(ans, sum);
-            if(sum<0)sum=0;
             ans = max(ans, sum);
+            if(sum<0)sum=0;
+            
         }
         return ans;
     }
