7a238b4b7d42362351233840358141af5e7da5cd
amantayal2909 <76427320+amantayal2909@users.noreply.github.com>
2026-03-30 11:17:43 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 2840-check-if-strings-can-be-made-equal-with-operations-ii/2840-check-if-strings-can-be-made-equal-with-operations-ii.java
+++ 2840-check-if-strings-can-be-made-equal-with-operations-ii/2840-check-if-strings-can-be-made-equal-with-operations-ii.java
@@ -1,42 +1,34 @@
 class Solution {
     public boolean checkStrings(String s1, String s2) {
         int size = s1.length();
-        HashMap<Character,Integer> map = new HashMap<>();
+        
+        int fre[] = new int[26];
         for(int i=0;i<size;i=i+2){
             char ch = s1.charAt(i);
-            if(map.containsKey(ch)){
-                map.put(ch,map.get(ch)+1);
-            }
-            else{
-                map.put(ch,1);
-            }
+            fre[ch-'a']++;
         }
 
         for(int i=0;i<size;i=i+2){
             char ch = s2.charAt(i);
-            if(map.containsKey(ch) && map.get(ch)>0){
-                map.put(ch,map.get(ch)-1);
+            if(fre[ch-'a']>0){
+                fre[ch-'a']--;
             }
             else{
                 return false;
             }
         }
-        map.clear();
+        
+        Arrays.fill(fre, 0);
 
         for(int i=1;i<size;i=i+2){
             char ch = s1.charAt(i);
-            if(map.containsKey(ch)){
-                map.put(ch,map.get(ch)+1);
-            }
-            else{
-                map.put(ch,1);
-            }
+            fre[ch-'a']++;
         }
 
         for(int i=1;i<size;i=i+2){
             char ch = s2.charAt(i);
-            if(map.containsKey(ch) && map.get(ch)>0){
-                map.put(ch,map.get(ch)-1);
+            if(fre[ch-'a']>0){
+                fre[ch-'a']--;
             }
             else{
                 return false;
@@ -48,3 +40,57 @@ class Solution {
         return true;
     }
 }
+
+
+
+
+// class Solution {
+//     public boolean checkStrings(String s1, String s2) {
+//         int size = s1.length();
+//         HashMap<Character,Integer> map = new HashMap<>();
+//         for(int i=0;i<size;i=i+2){
+//             char ch = s1.charAt(i);
+//             if(map.containsKey(ch)){
+//                 map.put(ch,map.get(ch)+1);
+//             }
+//             else{
+//                 map.put(ch,1);
+//             }
+//         }
+
+//         for(int i=0;i<size;i=i+2){
+//             char ch = s2.charAt(i);
+//             if(map.containsKey(ch) && map.get(ch)>0){
+//                 map.put(ch,map.get(ch)-1);
+//             }
+//             else{
+//                 return false;
+//             }
+//         }
+//         map.clear();
+
+//         for(int i=1;i<size;i=i+2){
+//             char ch = s1.charAt(i);
+//             if(map.containsKey(ch)){
+//                 map.put(ch,map.get(ch)+1);
+//             }
+//             else{
+//                 map.put(ch,1);
+//             }
+//         }
+
+//         for(int i=1;i<size;i=i+2){
+//             char ch = s2.charAt(i);
+//             if(map.containsKey(ch) && map.get(ch)>0){
+//                 map.put(ch,map.get(ch)-1);
+//             }
+//             else{
+//                 return false;
+//             }
+//         }
+
+
+
+//         return true;
+//     }
+// }
