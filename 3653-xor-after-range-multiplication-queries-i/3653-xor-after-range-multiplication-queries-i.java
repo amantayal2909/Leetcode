9c9f2fbc40d401d173d4e594b308f844bd5efe47
amantayal2909 <76427320+amantayal2909@users.noreply.github.com>
2026-04-08 10:04:26 +0530

    Time: 59 ms (88%), Space: 47.8 MB (69.33%) - LeetHub

---
+++ 3653-xor-after-range-multiplication-queries-i/3653-xor-after-range-multiplication-queries-i.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int xorAfterQueries(int[] nums, int[][] q) {
+        int q_size = q.length;
+        int ans = 0;
+        int mod = 1000000007;
+        for(int i=0; i<q_size; i++){
+            int l= q[i][0];
+            int r = q[i][1];
+            int k = q[i][2];
+            int v = q[i][3];
+
+            for(int j =l; j<=r; j=j+k){
+                nums[j] = (int)(((long)nums[j] * v) % (mod));
+            }
+        }
+
+        for(int i=0; i<nums.length; i++){
+            ans = ans ^ nums[i];
+        }
+
+        return ans;
+
+    }
+}
