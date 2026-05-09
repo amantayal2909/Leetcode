92ff2b413f4ea2cb225db079c53e146f558477ca
amantayal2909 <76427320+amantayal2909@users.noreply.github.com>
2026-05-09 23:57:09 +0530

    Time: 44 ms (69.14%), Space: 115.7 MB (28.5%) - LeetHub

--- 1722-minimize-hamming-distance-after-swap-operations/1722-minimize-hamming-distance-after-swap-operations.java
+++ 1722-minimize-hamming-distance-after-swap-operations/1722-minimize-hamming-distance-after-swap-operations.java
@@ -1,88 +1,84 @@
 class Solution {
-    class DSU{
-       int parent[];
-       int rank[];
-       int n;
+    public class DSU{
+        int parent[];
+        int rank[];
+        int n;
 
-       public DSU(int size){
-        parent = new int[size];
-        this.n = size;
-        rank = new int[size];
-        for(int i=0 ;i<size; i++){
-            parent[i]=i;
-        }
-       } 
+        public DSU(int n){
+            parent = new int[n];
+            this.n = n;
+            rank = new int[n];
 
-       public int find_parent(int i){
-        if(parent[i]==i)return i;
-
-        return parent[i]=find_parent(parent[i]);
-       }
-
-        public void union_dsu(int i, int j){
-        int x = find_parent(i);
-        int y = find_parent(j);
-
-        if(x==y)return;
-
-        int rank_x= rank[x];
-        int rank_y= rank[y];
-
-        if(rank_x == rank_y){
-            parent[x] = y;
-            rank[y]++;
-
-        }else if(rank_x> rank_y){
-            parent[y] = x;
-
-        }else{
-            parent[x] = y;
+            for(int i=0; i<n; i++)parent[i]=i;
         }
 
-       }
+        public int find_parent(int x){
+            if(parent[x] == x) return x;
 
+            return parent[x] = find_parent(parent[x]);
+        }
 
+        public void union(int x, int y){
+            int parentx = find_parent(x);
+            int parenty = find_parent(y);
+
+            if(parentx==parenty) return;
+
+            int rankx = rank[parentx];
+            int ranky = rank[parenty];
+
+            if(rankx == ranky){
+                parent[parentx] = parenty;
+                rank[parenty]++;
+            }
+            else if(rankx > ranky){
+                parent[parenty] = parentx;
+                
+            }
+            else{
+                parent[parentx] = parenty;
+            }
+        }
     }
     public int minimumHammingDistance(int[] source, int[] target, int[][] allowedSwaps) {
-        int size = source.length;
-        int ans=0;
-        DSU d = new DSU(size);
+        int n = source.length;
+        int ans = 0;
 
+        DSU d1 = new DSU(n);
         for(int i=0; i<allowedSwaps.length; i++){
-            d.union_dsu(allowedSwaps[i][0], allowedSwaps[i][1]);
+            d1.union(allowedSwaps[i][0], allowedSwaps[i][1]);
         }
 
-        HashMap <Integer, HashMap<Integer,Integer>> map = new HashMap<>();
-        for(int i=0 ;i<source.length; i++){
-            int val = source[i];
-            int parent = d.find_parent(i);
+        HashMap<Integer, HashMap<Integer,Integer>> group = new HashMap<>();
 
-            if(!map.containsKey(parent)){
-                map.put(parent,new HashMap<>());
+        for(int i=0; i<n; i++){
+            int value = source[i];
+            int parent = d1.find_parent(i);
+
+            if(!group.containsKey(parent))
+                group.put(parent, new HashMap<>());
+
+            HashMap<Integer, Integer> inside = group.get(parent);
+
+            inside.put(value, inside.getOrDefault(value,0)+1);
+        }
+
+        for(int i=0; i<n; i++){
+            int value = target[i];
+            int parent = d1.find_parent(i);
+
+            HashMap<Integer, Integer> inside = group.get(parent);
+
+            if(inside.containsKey(value) && inside.get(value)> 0){
+                inside.put(value, inside.get(value)-1 );
             }
-
-            HashMap<Integer,Integer> inside = map.get(parent);
-            inside.put(val, inside.getOrDefault(val, 0) + 1);
+            else ans++;
+            
         }
 
-        for(int i=0 ;i<source.length; i++){
-            int val = target[i];
-            int parent = d.find_parent(i);
-
-            HashMap<Integer,Integer> inside = map.get(parent);
-
-            if(inside.containsKey(val) && inside.get(val) >0)
-            {
-               inside.put(val, inside.get(val) - 1); 
-            }
-            else
-            {
-                ans ++;
-            }  
-
-        }
 
 
         return ans;
+
     }
 }
