aa68be955824e40b76c690ad5f3f1c57b5b64f08
amantayal2909 <76427320+amantayal2909@users.noreply.github.com>
2026-05-09 22:31:20 +0530

    Time: 24 ms (50.73%), Space: 120.1 MB (50.33%) - LeetHub

--- 2615-sum-of-distances/2615-sum-of-distances.java
+++ 2615-sum-of-distances/2615-sum-of-distances.java
@@ -1,32 +1,52 @@
 class Solution {
     public long[] distance(int[] nums) {
         int n = nums.length;
-        long[] ans = new long[n];
+        HashMap<Integer, List<Integer>> map = new HashMap<>();
 
-        Map<Integer, List<Integer>> mp = new HashMap<>();
+        for(int i =0; i<n; i++){
+            if(!map.containsKey(nums[i]))
+                map.put(nums[i], new ArrayList<>());
 
-        for (int i = 0; i < n; i++) {
-            mp.computeIfAbsent(nums[i], k -> new ArrayList<>()).add(i);
+            map.get(nums[i]).add(i);
         }
 
-        for (List<Integer> pos : mp.values()) {
+        long sum = 0;
+        long ans[] = new long[n];
 
-            long sum = 0;
-            for (int x : pos) sum += x;
+        for(Map.Entry<Integer, List<Integer>> entry: map.entrySet()){
 
-            long leftSum = 0;
-            int m = pos.size();
-
-            for (int i = 0; i < m; i++) {
-                long rightSum = sum - leftSum - pos.get(i);
-
-                long left  = (long) pos.get(i) * i - leftSum;
-                long right = rightSum - (long) pos.get(i) * (m - i - 1);
-
-                ans[pos.get(i)] = left + right;
-
-                leftSum += pos.get(i);
+            List<Integer> index = entry.getValue();
+            if(index.size()==1) 
+            {
+                ans[index.get(0)] = 0;
+                continue;
             }
+
+            int size = index.size();
+            long prefix[] = new long[size];
+            long suffix[] = new long[size];
+
+            prefix[0]= index.get(0);
+            for(int i=1; i<size; i++) prefix[i] = prefix[i-1] + index.get(i);
+
+            suffix[size-1]= index.get(size-1);
+            for(int i=size-2; i>=0; i--) suffix[i] = suffix[i+1] + index.get(i);
+
+
+            for(int i=0; i<size; i++){
+                int val = index.get(i);
+
+                long left = 0;
+                long right = 0;
+                if(i>0)
+                    left = ((long)i*val) - prefix[i-1];
+                
+                if(i<size-1)
+                    right = suffix[i+1] - (long)(size-1-i)*val;
+
+                ans[val] = left + right;
+            }
+
         }
 
         return ans;
