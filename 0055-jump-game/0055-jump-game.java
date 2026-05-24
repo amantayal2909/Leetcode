49e13ebe0f15687901bc034cd49fff9c45477b7c
amantayal2909 <76427320+amantayal2909@users.noreply.github.com>
2026-05-24 15:48:12 +0530

    Time: 2 ms (87.83%), Space: 48.2 MB (8.76%) - LeetHub

--- 0055-jump-game/0055-jump-game.java
+++ 0055-jump-game/0055-jump-game.java
@@ -1,28 +1,49 @@
-class Solution {
-    public boolean canJump(int[] nums) {
-        int size = nums.length;
-        if(size == 1) return true;
+// class Solution {
+//     public boolean canJump(int[] nums) {
+//         int size = nums.length;
+//         if(size == 1) return true;
 
-        int arr[] = new int[size];
-        Arrays.fill(arr,0);
-        arr[0] = 1;
+//         int arr[] = new int[size];
+//         Arrays.fill(arr,0);
+//         arr[0] = 1;
 
 
-        for(int i=0; i<size; i++){
+//         for(int i=0; i<size; i++){
 
-            if(arr[i]==0) return false;
+//             if(arr[i]==0) return false;
 
-            int val = nums[i];
+//             int val = nums[i];
 
-            for(int j=1; j<=val && (i+j)<=size-1; j++){
-                if((i+j) == size-1)
-                    return true;
-                arr[i+j]++;
-            }
+//             for(int j=1; j<=val && (i+j)<=size-1; j++){
+//                 if((i+j) == size-1)
+//                     return true;
+//                 arr[i+j]++;
+//             }
 
-        }
+//         }
 
     
-        return false;
+//         return false;
+//     }
+// }
+
+
+//      N^2
+
+
+class Solution {
+    public boolean canJump(int[] nums) {
+
+        int size = nums.length;
+        if(size == 1) return true;
+        int possible = 0;
+
+        for(int i=0; i<size; i++){
+            if(i>possible) return false;
+            possible = Math.max(possible, i+nums[i]);
+
+            if(possible>=size-1) return true;
+        }
+        return true;
     }
 }
