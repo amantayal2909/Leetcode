ddab9d29bdf55a18fb1e2b00fdb606199c2df67e
amantayal2909 <76427320+amantayal2909@users.noreply.github.com>
2026-05-24 18:27:49 +0530

    Time: 48 ms (84.11%), Space: 79.2 MB (76.01%) - LeetHub

--- 1345-jump-game-iv/1345-jump-game-iv.java
+++ 1345-jump-game-iv/1345-jump-game-iv.java
@@ -1,61 +1,70 @@
 class Solution {
-    public int minJumps(int[] arr) {
+    public int minJumps(int[] nums) {
+        int size = nums.length;
+        if(size == 1)return 0;
 
-        int n = arr.length;
+        boolean visit[] = new boolean[size];
+        Arrays.fill(visit,false);
 
-        if (n == 1) return 0;
+        HashMap<Integer,List<Integer>> map = new HashMap<>();
 
-        HashMap<Integer, List<Integer>> map = new HashMap<>();
-
-        for (int i = 0; i < n; i++) {
-            map.computeIfAbsent(arr[i], k -> new ArrayList<>()).add(i);
-        }
-
-        Queue<Integer> queue = new LinkedList<>();
-
-        boolean[] visited = new boolean[n];
-
-        queue.offer(0);
-        visited[0] = true;
-
-        int steps = 0;
-
-        while (!queue.isEmpty()) {
-
-            int size = queue.size();
-
-            while (size-- > 0) {
-
-                int idx = queue.poll();
-
-                if (idx == n - 1) {
-                    return steps;
-                }
-
-                if (idx - 1 >= 0 && !visited[idx - 1]) {
-                    visited[idx - 1] = true;
-                    queue.offer(idx - 1);
-                }
-
-                if (idx + 1 < n && !visited[idx + 1]) {
-                    visited[idx + 1] = true;
-                    queue.offer(idx + 1);
-                }
-
-                for (int nextIdx : map.get(arr[idx])) {
-
-                    if (!visited[nextIdx]) {
-                        visited[nextIdx] = true;
-                        queue.offer(nextIdx);
-                    }
-                }
-
-                map.get(arr[idx]).clear();
+        for(int i=0; i<size; i++){
+            if(!map.containsKey(nums[i])){
+                map.put(nums[i], new ArrayList<>());
             }
 
-            steps++;
+            map.get(nums[i]).add(i);
+        }
+        int jump = 0;
+
+        Queue<Integer> q = new ArrayDeque<>();
+        int start = 0;
+        q.add(start);
+        visit[start]= true;
+
+        while(!q.isEmpty()){
+            int qsize = q.size();
+
+            for(int i=0;i<qsize; i++){
+                int index = q.poll();
+                int val = nums[index];
+
+                int right_index = index +1;
+                int left_index = index -1;
+
+                if(right_index == size - 1  || left_index == size-1) return jump+1;
+                if(right_index <size && visit[right_index]==false){
+                    q.add(right_index);
+                    visit[right_index] = true;
+                }
+
+                if(left_index >=0 && visit[left_index]==false){
+                    q.add(left_index);
+                    visit[left_index] = true;
+                }
+
+                if(map.containsKey(val)){
+                    List<Integer> arr = map.get(val);
+
+                    for(int j=0; j<arr.size(); j++){
+                        if(arr.get(j) == index) continue;
+
+                        int map_index = arr.get(j);
+                        if(map_index == size-1)return jump+1;
+
+                        if(visit[map_index] == false){
+                            q.add(map_index);
+                            visit[map_index] = true;
+                        }
+                    }
+
+                    map.remove(val);
+                }
+
+            }
+            jump++;
         }
 
-        return -1;
+        return jump;
     }
 }
