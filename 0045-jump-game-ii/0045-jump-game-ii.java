47be3f9dd863c8d12b741b7be10d077159bfc6d0
amantayal2909 <76427320+amantayal2909@users.noreply.github.com>
2026-05-24 16:58:30 +0530

    Time: 1 ms (99.58%), Space: 47.4 MB (37.27%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.java
+++ 0045-jump-game-ii/0045-jump-game-ii.java
@@ -1,40 +1,67 @@
+// class Solution {
+//     public int jump(int[] nums) {
+//         int size = nums.length;
+//         if(size == 1)return 0;
+
+//         int arr[] = new int[size];
+//         Arrays.fill(arr,0);
+//         arr[0] = 1;
+
+//         Queue<Integer> q = new ArrayDeque<>();
+//         q.add(0);
+//         int jump = 0;
+
+//         while(!q.isEmpty()){
+
+//             int qsize = q.size();
+//             for(int i=0; i<qsize; i++){
+
+//                 int index = q.poll();
+//                 int val = nums[index];
+
+//                 for(int j=1; j<=val && (index+j)<=size-1; j++){
+//                     if(index + j == size - 1)
+//                         return jump + 1;
+
+//                     if(arr[index+j] == 0){    
+//                         arr[index+j]=1;
+//                         q.add(index+j);
+//                     }
+
+//                 }
+
+//             }
+//             jump++;
+
+//         }
+
+//         return -1;
+//     }
+// }
+
+//        N^2
+
+
+
 class Solution {
     public int jump(int[] nums) {
+
         int size = nums.length;
-        if(size == 1)return 0;
-
-        int arr[] = new int[size];
-        Arrays.fill(arr,0);
-        arr[0] = 1;
-
-        Queue<Integer> q = new ArrayDeque<>();
-        q.add(0);
+        if(size == 1) return 0;
+        int possible = 0;
         int jump = 0;
+        int end = 0;
 
-        while(!q.isEmpty()){
 
-            int qsize = q.size();
-            for(int i=0; i<qsize; i++){
-
-                int index = q.poll();
-                int val = nums[index];
-
-                for(int j=1; j<=val && (index+j)<=size-1; j++){
-                    if(index + j == size - 1)
-                        return jump + 1;
-
-                    if(arr[index+j] == 0){    
-                        arr[index+j]=1;
-                        q.add(index+j);
-                    }
-
-                }
-
+        for(int i=0; i<size-1; i++){
+            
+            possible = Math.max(possible, i+nums[i]);
+            if(i == end){
+                jump++;
+                end = possible;
             }
-            jump++;
 
         }
-
-        return -1;
+        return jump;
     }
 }
