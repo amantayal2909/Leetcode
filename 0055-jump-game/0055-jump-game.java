18bd9dc6962fd74daa1d1099999a59194b276cfd
amantayal2909 <76427320+amantayal2909@users.noreply.github.com>
2026-05-24 15:35:31 +0530

    Time: 52 ms (12%), Space: 47.8 MB (60.92%) - LeetHub

---
+++ 0055-jump-game/0055-jump-game.java
@@ -0,0 +1,28 @@
+class Solution {
+    public boolean canJump(int[] nums) {
+        int size = nums.length;
+        if(size == 1) return true;
+
+        int arr[] = new int[size];
+        Arrays.fill(arr,0);
+        arr[0] = 1;
+
+
+        for(int i=0; i<size; i++){
+
+            if(arr[i]==0) return false;
+
+            int val = nums[i];
+
+            for(int j=1; j<=val ; j++){
+                if((i+j) == size-1)
+                    return true;
+                arr[i+j]++;
+            }
+
+        }
+
+    
+        return false;
+    }
+}
